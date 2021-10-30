4813b110008d8411223841eb4eb784aa12f26d7c
weichweich <14820950+weichweich@users.noreply.github.com>
2021-10-30 13:22:33 +0200

    tests&bench

--- ascii85/benches/benchmarks.rs
+++ ascii85/benches/benchmarks.rs
@@ -9,7 +9,7 @@ use pdf_ascii85::decode;
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("encode-empty", |b| b.iter(|| decode(b"<~~>")));
 
-    let encoded = r#"<~9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,
+    let encoded = r#"9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,
     O<DJ+*.@<*K0@<6L(Df-\0Ec5e;DffZ(EZee.Bl.9pF"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY
     i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa
     l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G
--- ascii85/src/lib.rs
+++ ascii85/src/lib.rs
@@ -110,26 +110,35 @@ pub fn encode(data: &[u8]) -> Vec<u8> {
     buf
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE_CODEC: &str = r#"9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,
+    O<DJ+*.@<*K0@<6L(Df-\0Ec5e;DffZ(EZee.Bl.9pF"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY
+    i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa
+    l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G
+    >uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>"#;
+    const EXAMPLE_PLAIN: &[u8; 269] = b"Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+
     #[test]
     fn successfull_decode() {
         let tests = vec![
-            (&b""[..], &"<~~>"[..]),
+            // (&b""[..], &"<~~>"[..]),
             (&b""[..], &"~>"[..]),
-            (&b""[..], &"<~"[..]),
-            (&b"M"[..], &"9`"[..]),
-            (&b"Ma"[..], &"9jn"[..]),
-            (&b"Man"[..], &"9jqo"[..]),
-            (&b"Man "[..], &"9jqo^"[..]),
-            (&b"Man X"[..], &"9jqo^=9"[..]),
-            (&[0; 4], &"z"[..]),
-            (&[0; 4], &"<~z"[..]),
+            // (&b""[..], &"<~"[..]),
+            (&b"M"[..], &"9`~>"[..]),
+            (&b"Ma"[..], &"9jn~>"[..]),
+            (&b"Man"[..], &"9jqo~>"[..]),
+            (&b"Man "[..], &"9jqo^~>"[..]),
+            (&b"Man X"[..], &"9jqo^=9~>"[..]),
             (&[0; 4], &"z~>"[..]),
-            (&[0; 4], &"<~z~>"[..]),
-            (&[0; 16], &"zzzz"[..]),
+            // (&[0; 4], &"<~z"[..]),
+            (&[0; 4], &"z~>"[..]),
+            // (&[0; 4], &"<~z~>"[..]),
+            (&[0; 16], &"zzzz~>"[..]),
+            (EXAMPLE_PLAIN, EXAMPLE_CODEC),
         ];
 
         for (i, (plain, codec)) in tests.into_iter().enumerate() {
