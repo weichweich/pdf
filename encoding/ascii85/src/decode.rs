016c21c4fa2393c6c48bd8678beacbf5705de8cf
weichweich <14820950+weichweich@users.noreply.github.com>
2021-10-31 20:13:36 +0100

    document decode

--- encoding/ascii85/src/decode.rs
+++ encoding/ascii85/src/decode.rs
@@ -1,4 +1,4 @@
-use crate::{END_SEQUENCE, NULL_WORD, START_SEQUENCE, SYM_NULL};
+use crate::{END_SEQUENCE, NULL_CHAR, NULL_WORD, START_SEQUENCE};
 
 /// Maps an ASCII character to a number
 const fn to_number(byte: u8) -> Option<u8> {
@@ -9,47 +9,73 @@ const fn to_number(byte: u8) -> Option<u8> {
 }
 
 /// Decodes 5 ASCII85 bytes to 4 bytes.
-/// 
+///
 /// Assumes that the 5 bytes are already mapped to numbers using `to_number`.
-const fn decode_word([a, b, c, d, e]: [u8; 5]) -> [u8; 4] {
-    let q = (((a as u32 * 85 + b as u32) * 85 + c as u32) * 85 + d as u32) * 85 + e as u32;
+const fn decode_word(word: [u8; 5]) -> [u8; 4] {
+    let mut q = word[0] as u32;
+    q = q * 85 + word[1] as u32;
+    q = q * 85 + word[2] as u32;
+    q = q * 85 + word[3] as u32;
+    q = q * 85 + word[4] as u32;
     q.to_be_bytes()
 }
 
-/// Decodes ASCII85 encoded data.
-/// 
-/// The start (`<~`) and end (~>) are optional.
-/// 
-pub fn decode(mut data: &[u8]) -> Result<Vec<u8>, ()> {
-    data.strip_prefix(START_SEQUENCE)
-        .map(|stripped| data = stripped);
-    data.strip_suffix(END_SEQUENCE)
-        .map(|stripped| data = stripped);
+/// Fills the byte buffer using the iterator. Returns the number of elements that where copied to the buffer.
+fn fill_from_iter(stream: &mut impl Iterator<Item = u8>, buf: &mut [u8]) -> usize {
+    for (i, digit) in buf.iter_mut().enumerate() {
+        if let Some(b) = stream.next() {
+            *digit = b;
+        } else {
+            // the index where the iterator ended, index of the last byte written plus 1.
+            return i;
+        }
+    }
+    buf.len()
+}
 
-    let mut stream = data.iter().filter(|&b| !b.is_ascii_whitespace());
+/// Decodes ASCII85 encoded data.
+///
+/// The start (`<~`) and end (`~>`) are optional.
+/// The character `z` is interpreted as four zero bytes.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let encoded = "<~9jqo~>";
+/// let decoded = decode(encoded.as_bytes());
+/// 
+/// assert!(decoded.is_ok());
+/// assert_eq!(String::from_utf8(decoded.unwrap()), "Man".to_string());
+/// ```
+pub fn decode(mut data: &[u8]) -> Result<Vec<u8>, ()> {
+    if let Some(stripped) = data.strip_prefix(START_SEQUENCE) {
+        data = stripped;
+    }
+    if let Some(stripped) = data.strip_suffix(END_SEQUENCE) {
+        data = stripped;
+    }
+
+    let mut stream = data.iter().copied().filter(|&b| !b.is_ascii_whitespace());
 
     let mut out = Vec::with_capacity((data.len() + 4) / 5 * 4);
 
-
     let (tail_len, tail) = loop {
         match stream.next() {
-            Some(&NULL_WORD) => out.extend_from_slice(&[0; 4]),
+            Some(NULL_WORD) => out.extend_from_slice(&[0; 4]),
 
-            Some(&a) => {
-                let a = to_number(a).ok_or(())?;
-                let (b, c, d, e) = match (
-                    stream.next().map(|n| to_number(*n)).flatten(),
-                    stream.next().map(|n| to_number(*n)).flatten(),
-                    stream.next().map(|n| to_number(*n)).flatten(),
-                    stream.next().map(|n| to_number(*n)).flatten(),
-                ) {
-                    (Some(b), Some(c), Some(d), Some(e)) => (b, c, d, e),
-                    (None, _, _, _) => break (1, [a, SYM_NULL, SYM_NULL, SYM_NULL, SYM_NULL]),
-                    (Some(b), None, _, _) => break (2, [a, b, SYM_NULL, SYM_NULL, SYM_NULL]),
-                    (Some(b), Some(c), None, _) => break (3, [a, b, c, SYM_NULL, SYM_NULL]),
-                    (Some(b), Some(c), Some(d), None) => break (4, [a, b, c, d, SYM_NULL]),
-                };
-                out.extend_from_slice(&decode_word([a, b, c, d, e]));
+            Some(a) => {
+                let mut buf = [NULL_CHAR; 5];
+                buf[0] = a;
+
+                let copied = fill_from_iter(&mut stream, &mut buf[1..]);
+                for digit in &mut buf {
+                    *digit = to_number(*digit).ok_or(())?;
+                }
+                if copied < 4 {
+                    // The buffer was not filled. The stream
+                    break (copied + 1, buf);
+                }
+                out.extend_from_slice(&decode_word(buf));
             }
             None => break (0, [0; 5]),
         }
