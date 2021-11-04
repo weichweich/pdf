6e18492031748b2dd5f74ce8c8d607090aa0e0aa
weichweich <14820950+weichweich@users.noreply.github.com>
2021-11-04 22:08:49 +0100

    fix overflow error & efficient error index reporting

--- encoding/ascii85/src/decode.rs
+++ encoding/ascii85/src/decode.rs
@@ -1,12 +1,20 @@
 use crate::{END_SEQUENCE, NULL_CHAR, NULL_WORD, START_SEQUENCE};
 
 #[derive(Copy, Eq, PartialEq, Clone, Debug)]
-pub struct Ascii85DecodeError {
+pub struct DecodeError {
     /// The index of the character that caused the error.
     error_index: usize,
 }
 
-impl Ascii85DecodeError {
+impl DecodeError {
+    pub fn new_with_index(start: &u8, error: &u8) -> Self {
+        let ptr_start: *const u8 = start;
+        let ptr_error: *const u8 = error;
+
+        Self {
+            error_index: (ptr_error as usize) - (ptr_start as usize),
+        }
+    }
     /// Up to which index the buffer contained valid ASCII85 encoded data.
     pub fn valid_up_to(&self) -> usize {
         self.error_index.saturating_sub(1)
@@ -24,21 +32,21 @@ const fn to_number(byte: u8) -> Option<u8> {
 /// Decodes 5 ASCII85 bytes to 4 bytes.
 ///
 /// Assumes that the 5 bytes are already mapped to numbers using `to_number`.
-const fn decode_word(word: [u8; 5]) -> [u8; 4] {
+fn decode_word(word: [u8; 5]) -> Option<[u8; 4]> {
     let mut q = word[0] as u32;
     q = q * 85 + word[1] as u32;
     q = q * 85 + word[2] as u32;
     q = q * 85 + word[3] as u32;
-    q = q * 85 + word[4] as u32;
-    q.to_be_bytes()
+    q = q.checked_mul(85)?.checked_add(word[4] as u32)?;
+    Some(q.to_be_bytes())
 }
 
 /// Fills the byte buffer using the iterator. Returns the number of elements
 /// that where copied to the buffer.
-fn fill_from_iter(stream: &mut impl Iterator<Item = (usize, u8)>, buf: &mut [u8]) -> usize {
+fn fill_from_iter<'a>(stream: &mut impl Iterator<Item = &'a u8>, buf: &mut [u8]) -> usize {
     for (i, digit) in buf.iter_mut().enumerate() {
-        if let Some(b) = stream.next() {
-            *digit = b.1;
+        if let Some(&b) = stream.next() {
+            *digit = b;
         } else {
             // the index where the iterator ended, index of the last byte written plus 1.
             return i;
@@ -62,7 +70,16 @@ fn fill_from_iter(stream: &mut impl Iterator<Item = (usize, u8)>, buf: &mut [u8]
 ///
 /// assert_eq!(String::from_utf8(decoded), Ok("Man".to_string()));
 /// ```
-pub fn decode(mut data: &[u8]) -> Result<Vec<u8>, Ascii85DecodeError> {
+pub fn decode(mut data: &[u8]) -> Result<Vec<u8>, DecodeError> {
+    // get ref to first element or return empty vec if data is empty. We use the ref
+    // to the first element in case there is an invalid character in the data. The
+    // first element is then used to calculate the index of the char
+    let first = if let Some(first) = data.get(0) {
+        first
+    } else {
+        return Ok(Vec::with_capacity(0));
+    };
+
     if let Some(stripped) = data.strip_prefix(START_SEQUENCE) {
         data = stripped;
     }
@@ -70,41 +87,32 @@ pub fn decode(mut data: &[u8]) -> Result<Vec<u8>, Ascii85DecodeError> {
         data = stripped;
     }
 
-    let mut stream = data
-        .iter()
-        .copied()
-        .enumerate()
-        .filter(|&(_, b)| !b.is_ascii_whitespace());
+    let mut stream = data.iter().filter(|b| !b.is_ascii_whitespace());
 
     let mut out = Vec::with_capacity((data.len() + 4) / 5 * 4);
 
-    let (tail_len, tail) = loop {
+    loop {
         match stream.next() {
-            Some((_, NULL_WORD)) => out.extend_from_slice(&[0; 4]),
+            Some(&NULL_WORD) => out.extend_from_slice(&[0; 4]),
 
-            Some((index, a)) => {
+            Some(a) => {
                 let mut buf = [NULL_CHAR; 5];
-                buf[0] = a;
+                buf[0] = *a;
 
                 let copied = fill_from_iter(&mut stream, &mut buf[1..]);
                 for digit in &mut buf {
-                    *digit = to_number(*digit).ok_or_else(|| Ascii85DecodeError { error_index: index })?;
+                    *digit = to_number(*digit).ok_or_else(|| DecodeError::new_with_index(first, a))?;
                 }
+                let word = decode_word(buf).ok_or_else(|| DecodeError::new_with_index(first, a))?;
                 if copied < 4 {
                     // The buffer was not filled. The stream stopped while copying.
-                    // Only `copied` elements where put in the buffer by `fill_from_iter`. One
-                    // element was put in manually.
-                    break (copied + 1, buf);
+                    out.extend_from_slice(&word[..copied]);
+                } else {
+                    out.extend_from_slice(&word);
                 }
-                out.extend_from_slice(&decode_word(buf));
             }
-            None => break (0, [0; 5]),
+            None => break,
         }
-    };
-
-    if tail_len > 0 {
-        let last = decode_word(tail);
-        out.extend_from_slice(&last[..tail_len - 1]);
     }
 
     Ok(out)
@@ -123,5 +131,26 @@ mod tests {
             assert!(decoded.is_ok(), "Error in test case #{} ({})", i, codec);
             assert_eq!(plain, decoded.unwrap(), "Couldn't decode test case #{} ({})", i, codec);
         }
+    }
+
+    #[test]
+    fn correct_error_index() {
+        let mut example = crate::tests::EXAMPLE_CODEC.to_owned();
+        // replace a word in ascii85 with a word that is out of bounds.
+        example.replace_range(9..14, "uuuuu");
+
+        // should report the index at which the error occurred (10 in this case)
+        let decoded = decode(example.as_bytes());
+        assert_eq!(decoded, Err(DecodeError { error_index: 10 }));
+
+        // should be able to decode up to the invalid word
+        let decoded = decode(&example.as_bytes()[..10]);
+        assert!(decoded.is_ok());
+    }
+
+    #[test]
+    fn errors_on_max_invalide() {
+        let decoded = decode(&[0x75, 0x75, 0x75, 0x75, 0x75]);
+        assert!(matches!(decoded, Err(DecodeError { error_index: _ })));
     }
 }
--- encoding/ascii85/src/lib.rs
+++ encoding/ascii85/src/lib.rs
@@ -27,7 +27,7 @@
 mod decode;
 mod encode;
 
-pub use decode::{decode, Ascii85DecodeError};
+pub use decode::{decode, DecodeError};
 pub use encode::encode;
 
 /// The character `u` in the ASCII table represents the null byte (0x00).
@@ -42,8 +42,8 @@ const END_SEQUENCE: &[u8; 2] = b"~>";
 
 #[cfg(test)]
 mod tests {
-    const EXAMPLE_CODEC: &str = r#"9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ+*.@<*K0@<6L(Df-\0Ec5e;DffZ(EZee.Bl.9pF"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>"#;
-    const EXAMPLE_PLAIN: &[u8; 269] = b"Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+    pub(crate) const EXAMPLE_CODEC: &str = r#"9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ+*.@<*K0@<6L(Df-\0Ec5e;DffZ(EZee.Bl.9pF"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>"#;
+    pub(crate) const EXAMPLE_PLAIN: &[u8; 269] = b"Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
 
     pub(crate) fn decode_samples() -> Vec<(&'static [u8], &'static str)> {
         return vec![
